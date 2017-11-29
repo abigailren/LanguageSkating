b8255de23582907cb759de52171ebb57e12bd1e4
Abigail Ren <abigailren@vassar.edu>
2017-11-28 23:35:05 -0500

    Added Germany subclass of Character

---
+++ LanguageSkating/src/main/java/verlierer/Germany.java
@@ -0,0 +1,11 @@
+package verlierer;
+import acm.graphics.*;
+
+public class Germany extends Character {
+
+
+    public Germany(){
+        super.appearance = new GImage("germany.png");
+        super.bio = new GLabel("I am Germany");
+    }
+}
