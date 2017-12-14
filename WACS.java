0c748d080389a17894a1f1ae89076c242b501cd3
Dakota <dakota.crugnale@yahoo.com>
2017-12-14 13:54:09 -0500

    String Arrays

---
+++ WACS.java
@@ -0,0 +1,45 @@
+
+package wordarrays;
+
+
+public class WordArrays {
+    public static String[] words = new String[] {"Hello", "Goodbye", "Yes",
+     "No", "Thanks", "Good", "Bad", "Not", "Happy", 
+     "Sad", "Who", "What", "When", "Where", "Why", "How"};
+ 
+  public static String[] spanishWords = new String[] {"Hola", "Adi\u00F3s", "S\u00ED",
+     "No", "Gracias", "Bueno/a", "Malo/a", "No", "contento/a", 
+     "triste", "Qui\u00E9n", "Qu\u00E9", "Cuando", "Donde", "Por Qu\u00E9", "Como"};
+  
+  public static String[] japanWords = new String[] {"\u3093\u306B\u3061\u3051", 
+      "\u3055\u3088\u306A\u3089", "\u306F\u3044", "\u3044\u3044\u3048", 
+      "\u3042\u308A\u304C\u3068\u3046", "\u3044\u3044", "\u308F\u308B\u3044", "Not", "Happy", 
+     "Sad", "Who", "What", "When", "Where", "Why", "How"};               
+ 
+  public static String[] germanyWords = new String[] {"Hallo", "Auf Wiedersehen", "Ja",
+     "Nien", "Danke", "Gut", "Schlecht", "Nicht", "gl\u00FCcklich", 
+     "traurig", "Wer", "Was", "Wo", "Wann", "Wieso", "Wie"};
+  
+  public static String[] chinaWords = new String[] {"\u4f60\u597d", "\u518d\u89c1", 
+     "\u662f", "\u6c92\u6709", "\u8C22\u8C22", "\u597d", "\u574f", "\u4e0d", 
+     "\u5feb\u6a02", "\u50b7\u5fc3", "\u8ab0", "\u4ec0\u9ebc", 
+     "\u4ec0\u9ebc\u6642\u5019", "\u54ea\u88e1", "\u70ba\u4ec0\u9ebc", 
+     "\u600e\u9ebc\u6a23"};
+
+    
+        public static void main(String[] args)  {
+        
+    System.out.println(spanishWords[4]);
+    System.out.println(words[4]);
+    System.out.println(japanWords[4]);
+    System.out.println(germanyWords[4]);
+    System.out.println(chinaWords[4]);    
+    
+
+
+
+
+       
+    }
+    
+}
